4cd6b107119ab2484b04f5c114f644c1a72499a2
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-11-21 14:27:14 +0700

    first commit

---
+++ Backend/config/vnpayConfig.js
@@ -0,0 +1,7 @@
+// config/vnpayConfig.js
+module.exports = {
+  vnp_TmnCode: "A14ZG7RU",
+  vnp_HashSecret: "GWZVNUYKOESWJXIFUXGSWNQQLSJCPQGT",
+  vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
+  vnp_ReturnUrl: "http://localhost:3000/vnpay_return",
+};
